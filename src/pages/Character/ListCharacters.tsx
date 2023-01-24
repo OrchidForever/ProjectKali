5d082b51d85604fd155d3840f984f4cd07447f58
Kai <orchidwrites@gmail.com>
2023-01-23 21:49:25 -0600

    add: adding tailwind, folk list

---
+++ src/pages/Character/ListCharacters.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../index.css';
+
+function ListCharacter() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+      List Character Page
+    </h1>
+  );  
+}
+
+export default ListCharacter;
