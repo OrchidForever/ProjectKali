7294ca7cd94c077434ead9529714dfb326da1ebc
Brenna Auker <brennaauker@gmail.com>
2023-01-25 20:29:22 -0600

    Add: Character Create, prettier, linting Refactor: Linting/Prettier

--- src/pages/Character/ListCharacters.tsx
+++ src/pages/Character/ListCharacters.tsx
@@ -1,20 +1,19 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
-import { useReadLocalStorage } from 'usehooks-ts';
-import '../../index.css';
-import { Button, Typography } from "@material-tailwind/react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { useReadLocalStorage } from 'usehooks-ts'
+import '../../index.css'
+import { Button, Typography } from '@material-tailwind/react'
+import { Link } from 'react-router-dom'
 
 function ListCharacter() {
-  const playerCharacters = useReadLocalStorage('playerCharacters');
+  const playerCharacters = useReadLocalStorage('playerCharacters')
   return (
     <div>
       <Typography variant="h1" color="deep-purple">
-        Number of Characters: {typeof playerCharacters == 'string' ? JSON.parse(playerCharacters).length : 0}
+        Number of Characters: {typeof playerCharacters === 'string' ? JSON.parse(playerCharacters).length : 0}
       </Typography>
       <Button variant="outlined" size="lg" color="deep-purple"><Link to='/create-character'>Create Character</Link></Button>
     </div>
-  );
+  )
 }
 
-export default ListCharacter;
+export default ListCharacter
--- src/types/folk.ts
+++ src/types/folk.ts
@@ -1,32 +1,32 @@
 export const folk = [
-    {
-        id: 1,
-        name: 'Kawayanon',
-        otherName: 'Bamboo Folk'
-    },
-    {
-        id: 2,
-        name: 'Iro-Iro',
-        otherName: 'Dog Folk'
-    },
-    {
-        id: 3,
-        name: 'Kamikam',
-        otherName: 'Cat Folk'
-    },
-    {
-        id: 4,
-        name: 'Laksaman',
-        otherName: 'Monkey Folk'
-    },
-    {
-        id: 5,
-        name: 'Pilandokan',
-        otherName: 'Deer Folk'
-    },
-    {
-        id: 6,
-        name: 'Pak-an',
-        otherName: 'Bird Folk'
-    }
+  {
+    id: 1,
+    name: 'Kawayanon',
+    otherName: 'Bamboo Folk'
+  },
+  {
+    id: 2,
+    name: 'Iro-Iro',
+    otherName: 'Dog Folk'
+  },
+  {
+    id: 3,
+    name: 'Kamikam',
+    otherName: 'Cat Folk'
+  },
+  {
+    id: 4,
+    name: 'Laksaman',
+    otherName: 'Monkey Folk'
+  },
+  {
+    id: 5,
+    name: 'Pilandokan',
+    otherName: 'Deer Folk'
+  },
+  {
+    id: 6,
+    name: 'Pak-an',
+    otherName: 'Bird Folk'
+  }
 ]
